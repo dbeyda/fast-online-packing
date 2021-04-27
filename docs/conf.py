572ac8f6e98e8a58c707268090d851f252a87b53
David Beyda <dbeyda@globo.com>
2021-04-27 13:57:48 -0300

    [update] Docs using Furo theme

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,8 @@ author = 'David'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
     'sphinx.ext.mathjax',
     'numpydoc']
 
@@ -49,7 +51,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'furo'
+html_title = project
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
